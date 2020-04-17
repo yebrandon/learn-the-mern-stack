d936d29042b56d11069daec3f26f9cd48d8db3af
brandon <contactbrandonye@gmail.com>
2020-04-17 17:46:02 -0400

    jwt auth backend

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-// String parser is depreciated
-
-module.exports = {
-  mongoURI:
-    "mongodb+srv://brandon:Brandonmongo526@mern-shopping-mfgtl.mongodb.net/test?retryWrites=true&w=majority",
-};
--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const path = require("path");
-
-const items = require("./routes/api/items");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const config = require('config');
 
 const app = express();
 
 // Bodyparser Middleware
-app.use(bodyParser.json());
+app.use(express.json());
+
+// Use Routes
+app.use('/api/items', require('./routes/api/items'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = config.get('mongoURI');
 
 // Connect to Mongo
 mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB connected..."))
-  .catch((err) => console.log(err));
-
-// Use routes
-app.use("/api/items", items);
+	.connect(db, { useNewUrlParser: true, useCreateIndex: true })
+	.then(() => console.log('MongoDB connected...'))
+	.catch(err => console.log(err));
 
 // Serve static assets if in production
 
-if (process.env.NODE_ENV == "production") {
-  //Set static folder
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV == 'production') {
+	//Set static folder
+	app.use(express.static('client/build'));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
 }
 
 const port = process.env.PORT || 5000;
