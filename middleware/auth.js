05cb4ddf8686671fcd4d77dfb686fa4a62df2083
brandon <contactbrandonye@gmail.com>
2020-04-17 21:34:11 -0400

    auth state

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,8 +5,9 @@ auth = (req, res, next) => {
 	const token = req.header('x-auth-token');
 
 	// Check for token
-	if (!token) res.status(401).json({ msg: 'No token, authorization denied' });
-
+	if (!token) {
+		return res.status(401).json({ msg: 'No token, authorization denied' });
+	}
 	try {
 		// Verify token
 		const decoded = jwt.verify(token, config.get('jwtSecret'));
