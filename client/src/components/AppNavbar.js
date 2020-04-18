82f1afa5733e1124c5bb345c486c79030ee0d02f
brandon <contactbrandonye@gmail.com>
2020-04-18 18:42:09 -0400

    login and access control

--- client/src/components/AppNavbar.js
+++ client/src/components/AppNavbar.js
@@ -1,6 +1,6 @@
 // Legacy Context API Warning
 
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import {
 	Collapse,
 	Navbar,
@@ -11,12 +11,19 @@ import {
 	NavLink,
 	Container
 } from 'reactstrap';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import RegisterModal from './auth/RegisterModal';
+import LoginModal from './auth/LoginModal';
 import Logout from './auth/Logout';
 
 class AppNavbar extends Component {
 	state = {
 		isOpen: false
+	};
+
+	static propTypes = {
+		auth: PropTypes.object.isRequired
 	};
 
 	toggle = () => {
@@ -26,6 +33,32 @@ class AppNavbar extends Component {
 	};
 
 	render() {
+		const { isAuthenticated, user } = this.props.auth;
+
+		const authLinks = (
+			<Fragment>
+				<NavItem>
+					<span className='navbar-text mr-3'>
+						<strong>{user ? `Welcome ${user.name}` : ''}</strong>
+					</span>
+				</NavItem>
+				<NavItem>
+					<Logout />
+				</NavItem>
+			</Fragment>
+		);
+
+		const guestLinks = (
+			<Fragment>
+				<NavItem>
+					<RegisterModal />
+				</NavItem>
+				<NavItem>
+					<LoginModal />
+				</NavItem>
+			</Fragment>
+		);
+
 		return (
 			<div>
 				<Navbar color='dark' dark expand='sm' className='mb-5'>
@@ -35,16 +68,11 @@ class AppNavbar extends Component {
 						<NavbarToggler onClick={this.toggle} />
 						<Collapse isOpen={this.state.isOpen} navbar>
 							<Nav className='ml-auto' navbar>
+								{isAuthenticated ? authLinks : guestLinks}
 								<NavItem>
 									<NavLink href='https://github.com/yebrandon/learn-the-mern-stack.git'>
 										Github
 									</NavLink>
-								</NavItem>
-								<NavItem>
-									<RegisterModal />
-								</NavItem>
-								<NavItem>
-									<Logout />
 								</NavItem>
 							</Nav>
 						</Collapse>
@@ -55,4 +83,8 @@ class AppNavbar extends Component {
 	}
 }
 
-export default AppNavbar;
+const mapStateToProps = (state) => ({
+	auth: state.auth
+});
+
+export default connect(mapStateToProps, null)(AppNavbar);
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,54 +1,65 @@
-import React, { Component } from "react";
-import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
-import { connect } from "react-redux";
-import { getItems, deleteItem } from "../actions/itemActions";
-import PropTypes from "prop-types";
+import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import { connect } from 'react-redux';
+import { getItems, deleteItem } from '../actions/itemActions';
+import PropTypes from 'prop-types';
 
 class ShoppingList extends Component {
-  componentDidMount() {
-    this.props.getItems();
-  }
+	static propTypes = {
+		getItems: PropTypes.func.isRequired,
+		item: PropTypes.object.isRequired,
+		isAuthenticated: PropTypes.bool
+	};
 
-  onDeleteClick = (id) => {
-    this.props.deleteItem(id);
-  };
+	componentDidMount() {
+		this.props.getItems();
+	}
 
-  render() {
-    const { items } = this.props.item;
-    return (
-      <Container>
-        <ListGroup>
-          <TransitionGroup className="shopping-list">
-            {items.map(({ _id, name }) => (
-              <CSSTransition key={_id} timeout={500} classNames="fade">
-                <ListGroupItem>
-                  <Button
-                    className="remove-btn"
-                    color="danger"
-                    size="sm"
-                    onClick={this.onDeleteClick.bind(this, _id)}
-                  >
-                    &times;
-                  </Button>
-                  {name}
-                </ListGroupItem>
-              </CSSTransition>
-            ))}
-          </TransitionGroup>
-        </ListGroup>
-      </Container>
-    );
-  }
+	onDeleteClick = (id) => {
+		this.props.deleteItem(id);
+	};
+
+	render() {
+		const { items } = this.props.item;
+		return (
+			<Container>
+				<ListGroup>
+					<TransitionGroup className='shopping-list'>
+						{items.map(({ _id, name }) => (
+							<CSSTransition
+								key={_id}
+								timeout={500}
+								classNames='fade'
+							>
+								<ListGroupItem>
+									{this.props.isAuthenticated ? (
+										<Button
+											className='remove-btn'
+											color='danger'
+											size='sm'
+											onClick={this.onDeleteClick.bind(
+												this,
+												_id
+											)}
+										>
+											&times;
+										</Button>
+									) : null}
+									{name}
+								</ListGroupItem>
+							</CSSTransition>
+						))}
+					</TransitionGroup>
+				</ListGroup>
+			</Container>
+		);
+	}
 }
 
-ShoppingList.propTypes = {
-  getItems: PropTypes.func.isRequired,
-  item: PropTypes.object.isRequired,
-};
-
 const mapStateToProps = (state) => ({
-  item: state.item,
+	item: state.item,
+	isAuthenticated: state.auth.isAuthenticated
 });
 
 export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList);
